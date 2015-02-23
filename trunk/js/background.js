633fc75f70f19f76e7d8769dfac7c86f1c1f3fb0
george.miscalencu@gmail.com <george.miscalencu@gmail.com@4b1be2a2-05d8-11df-87ec-d30288882566>
2015-02-23 11:58:59 +0000

    Updates to options page

--- trunk/js/background.js
+++ trunk/js/background.js
@@ -94,13 +94,6 @@ function goUpdateBadge(weatherObj) {
 		chrome.browserAction.setBadgeBackgroundColor({ color: [0, 153, 204, 255] });
 		chrome.browserAction.setTitle({ title: badgeTitle });
 
-		//if (weatherObj.Icon != "") {
-		//	chrome.browserAction.setIcon({ path: getSettings("imgLocation") + weatherObj.Icon.split("/")[weatherObj.Icon.split("/").length - 1] });
-		//}
-		//else {
-		//	chrome.browserAction.setIcon({ path: "images/icon.png" });
-		//}
-
 		$("#canvas").html(getIcon(weatherObj.ConditionCode, findIfIsDay(weatherObj)));
 		html2canvas($("#canvas"), {
 		    onrendered: function (canvas) {
